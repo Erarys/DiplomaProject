2c80cb49d9ec2e56de6c372e86aa91750f24fe82
Erarys <erarysabylkasym3@gmail.com@>
2025-03-22 19:06:48 +0500

    added static files

---
+++ smartsite/speech/static/speech/js/recording_script.js
@@ -0,0 +1,56 @@
+let mediaRecorder;
+let recordedChunks = [];
+
+const video = document.getElementById('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+// Получаем доступ к камере
+navigator.mediaDevices.getUserMedia({
+    video: {width: 640, height: 480},
+    audio: {
+        sampleRate: 48000,
+        channelCount: 2,
+        echoCancellation: false,
+        noiseSuppression: false,
+        autoGainControl: false
+    }
+})
+    .then(stream => {
+        video.srcObject = stream;
+        mediaRecorder = new MediaRecorder(stream, {
+            mimeType: 'video/webm; codecs=vp9,opus'
+        });
+
+        mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                recordedChunks.push(event.data);
+            }
+        };
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(recordedChunks, {type: 'video/webm'});
+            recordedChunks = [];
+
+            // Отправляем видео на сервер
+            const formData = new FormData();
+            formData.append('video', blob, 'recorded_video.webm');
+
+            fetch('http://127.0.0.1:8000/upload-photo/', {
+                method: 'POST',
+                body: formData
+            }).then(response => response.json())
+                .then(data => console.log(data));
+        };
+    })
+    .catch(err => console.error('Ошибка доступа к камере: ', err));
+
+startBtn.onclick = () => {
+    mediaRecorder.start();
+    console.log('Запись началась');
+};
+
+stopBtn.onclick = () => {
+    mediaRecorder.stop();
+    console.log('Запись остановлена и отправляется');
+};
