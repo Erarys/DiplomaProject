fd6baf681fd036733b68f6bc1498f88e9356d2ec
Erarys <erarysabylkasym3@gmail.com@>
2025-03-24 19:49:26 +0500

    Added background for main page

--- smartsite/speech/views.py
+++ smartsite/speech/views.py
@@ -1,49 +1,46 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import JsonResponse
-from django.conf import settings
-
-
-import os
-
-from speech.services.intelegence import generate_answer
-from speech.services.speech_recognition import recognize_audio
-from speech.services.speech import generate_speach
-from speech.services.emotion_recognition import detect_emotion_from_video
-from speech.services.separate_video_audio import extract_audio_from_video
-from speech.services.voice_emotion import voice_emotion_recognition
+# from django.http import JsonResponse
+# from django.conf import settings
+#
+#
+# import os
+#
+# from speech.services.intelegence import generate_answer
+# from speech.services.speech_recognition import recognize_audio
+# from speech.services.speech import generate_speach
+# from speech.services.emotion_recognition import detect_emotion_from_video
+# from speech.services.separate_video_audio import extract_audio_from_video
+# from speech.services.voice_emotion import voice_emotion_recognition
 
 class SpeechRecognition(View):
     def get(self, request):
         return render(request, 'speech/speech-record.html')
 
-    def post(self, request):
-        if request.method == 'POST':
-            video_file = request.FILES.get('video')
-            if video_file:
-                os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
-                file_path = os.path.join(settings.MEDIA_ROOT, video_file.name)
-                # print(file_path)
-                with open(file_path, 'wb+') as destination:
-                    for chunk in video_file.chunks():
-                        destination.write(chunk)
+    # def post(self, request):
+    #     if request.method == 'POST':
+    #         video_file = request.FILES.get('video')
+    #         if video_file:
+    #             os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+    #             file_path = os.path.join(settings.MEDIA_ROOT, video_file.name)
+    #             # print(file_path)
+    #             with open(file_path, 'wb+') as destination:
+    #                 for chunk in video_file.chunks():
+    #                     destination.write(chunk)
+    #
+    #             emotion = detect_emotion_from_video(file_path)
+    #             audio_bytes = extract_audio_from_video(file_path)
+    #             text = recognize_audio(audio_bytes)
+    #             voice_emotion = voice_emotion_recognition(audio_bytes)
+    #             message = text + f" Эмоция лица: {emotion}, Эмоций голоса {voice_emotion}"
+    #             print(message)
+    #
+    #             answer = generate_answer(message)
+    #             generate_speach(answer)
+    #
+    #             return JsonResponse({'status': 'success'})
+    #         return JsonResponse({'status': 'no file'}, status=400)
+    #
+    #     return JsonResponse({'status': 'invalid method'}, status=405)
 
-                emotion = detect_emotion_from_video(file_path)
-                audio_bytes = extract_audio_from_video(file_path)
-                text = recognize_audio(audio_bytes)
-                voice_emotion = voice_emotion_recognition(audio_bytes)
-                message = text + f" Эмоция лица: {emotion}, Эмоций голоса {voice_emotion}"
-                print(message)
 
-                answer = generate_answer(message)
-                generate_speach(answer)
-
-                return JsonResponse({'status': 'success'})
-            return JsonResponse({'status': 'no file'}, status=400)
-
-        return JsonResponse({'status': 'invalid method'}, status=405)
-
-
-if __name__ == "__main__":
-    data = extract_audio_from_video("../media/recorded_video.webm")
-    print(voice_emotion_recognition(data))
